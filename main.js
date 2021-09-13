d76834812e9fae85acc81e56828422a7dc02aa43
rounantj <48033669+rounantj@users.noreply.github.com>
2021-09-13 16:12:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import 'ol/ol.css';
+import ImageLayer from 'ol/layer/Image';
+import ImageMapGuide from 'ol/source/ImageMapGuide';
+import Map from 'ol/Map';
+import View from 'ol/View';
+
+const mdf = 'Library://Samples/Sheboygan/Maps/Sheboygan.MapDefinition';
+const agentUrl = 'https://mikenunn.net/mapguide/mapagent/mapagent.fcgi?';
+const bounds = [
+  -87.865114442365922, 43.665065564837931, -87.595394059497067,
+  43.823852564430069,
+];
+const map = new Map({
+  layers: [
+    new ImageLayer({
+      extent: bounds,
+      source: new ImageMapGuide({
+        projection: 'EPSG:4326',
+        url: agentUrl,
+        useOverlay: false,
+        metersPerUnit: 111319.4908, //value returned from mapguide
+        params: {
+          MAPDEFINITION: mdf,
+          FORMAT: 'PNG',
+          VERSION: '3.0.0',
+          USERNAME: 'OLGuest',
+          PASSWORD: 'olguest',
+        },
+        ratio: 2,
+      }),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [-87.7302542509315, 43.744459064634],
+    projection: 'EPSG:4326',
+    zoom: 12,
+  }),
+});
